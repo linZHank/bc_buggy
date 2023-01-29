ab2e69816e73e0fe3c8b3dad98eb4fbdc5be0e25
linzhank <linzhank@gmail.com>
2023-01-29 14:45:50 -0600

    rename test scripts

--- tests/motor.py
+++ tests/motor.py
@@ -1,13 +1,13 @@
 from gpiozero import PhaseEnableMotor
 from time import sleep
 
-throttle = PhaseEnableMotor(phase=24, enable=18)
+throttle = PhaseEnableMotor(phase=19, enable=26)
 print("Forward")
-throttle.forward(0.2)
+throttle.forward(0.25)
 sleep(4)
 print("Stop")
 throttle.stop()
 sleep(1)
 print("Backward")
-throttle.backward(0.2)
+throttle.backward(0.25)
 sleep(4)
